5d7c7222cf1bc5d6b5d6c2e5827256e797c184b9
Eduarda Ramos <2171347@my.ipleiria.pt>
2019-12-19 15:52:50 +0000

    Added getters and setters to class Presenca.

--- src/entities/Presenca.java
+++ src/entities/Presenca.java
@@ -3,9 +3,19 @@ package entities;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.Set;
 
+
+
 @Entity
+@Table(name = "PRESENCAS")
+@NamedQueries(
+        @NamedQuery(
+                name = "getAllPresencas",
+                query = "SELECT p FROM Presenca p ORDER BY p.id"
+        )
+)
 public class Presenca {
 
     @Id
@@ -29,7 +39,54 @@ public class Presenca {
     public Presenca() {
     }
 
-    public Presenca(Treino treino, Data dataTreino, Treinador treinador){
+    public Presenca(Treino treino, GregorianCalendar dataTreino, Treinador treinador){
+        this.treino = treino;
+        this.dataTreino = dataTreino;
+        this.treinador = treinador;
+        this.atletasPresentes = new HashSet<>();
+    }
 
+    public Treino getTreino() {
+        return treino;
+    }
+
+    public void setTreino(Treino treino) {
+        this.treino = treino;
+    }
+
+    public GregorianCalendar getDataTreino() {
+        return dataTreino;
+    }
+
+    public void setDataTreino(GregorianCalendar dataTreino) {
+        this.dataTreino = dataTreino;
+    }
+
+    public Set<Atleta> getAtletasPresentes() {
+        return atletasPresentes;
+    }
+
+    public void setAtletasPresentes(Set<Atleta> atletasPresentes) {
+        this.atletasPresentes = atletasPresentes;
+    }
+
+    public Treinador getTreinador() {
+        return treinador;
+    }
+
+    public void setTreinador(Treinador treinador) {
+        this.treinador = treinador;
+    }
+
+    public void addAtletaPresente (Atleta atleta){
+        if (!atletasPresentes.contains(atleta)){
+            atletasPresentes.add(atleta);
+        }
+    }
+
+    public void removeAtletaPresente (Atleta atleta){
+        if (atletasPresentes.contains(atleta)){
+            atletasPresentes.remove(atleta);
+        }
     }
 }
