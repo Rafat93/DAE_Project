646c89871169adceb64155d0dc415abdb2dc5cc7
Eduarda Ramos <2171347@my.ipleiria.pt>
2019-12-19 11:34:17 +0000

    Added getters and setters to Escalao.java

--- src/entities/Escalao.java
+++ src/entities/Escalao.java
@@ -35,4 +35,44 @@ public class Escalao {
         this.idadeMax = idadeMax;
         this.modalidade = modalidade ;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdadeMin() {
+        return idadeMin;
+    }
+
+    public void setIdadeMin(int idadeMin) {
+        this.idadeMin = idadeMin;
+    }
+
+    public int getIdadeMax() {
+        return idadeMax;
+    }
+
+    public void setIdadeMax(int idadeMax) {
+        this.idadeMax = idadeMax;
+    }
+
+    public Modalidade getModalidade() {
+        return modalidade;
+    }
+
+    public void setModalidade(Modalidade modalidade) {
+        this.modalidade = modalidade;
+    }
 }
