e7c8020154030cb6026dcb77395bf87d0115833e
Rafael Correia <2171295@my.ipleiria.pt>
2019-12-29 11:38:04 +0000

    Added Compra

---
+++ src/entities/Compra.java
@@ -0,0 +1,137 @@
+package entities;
+
+
+import enums.TipoPagamentoCompra;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.*;
+
+@Entity
+@Table(name="COMPRAS")
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllCompras",
+                query = "SELECT c FROM Compra c ORDER BY c.code" // JPQL
+        )
+})
+public class Compra {
+
+    @Id
+    private String code;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+
+    @NotNull
+    @ManyToMany
+    private List<Produto> produtos;
+
+    @NotNull
+    private GregorianCalendar dataCompra;
+
+    @NotNull
+    private TipoPagamentoCompra tipoPagamentoCompra;
+
+    @NotNull
+    @ManyToMany
+    private Set<Pagamento> pagamentos;
+
+    @NotNull
+    private double valorTotal;
+
+    @NotNull
+    private double valorPago;
+
+
+    public Compra() {
+        this.produtos = new LinkedList<>();
+        this.pagamentos = new HashSet<>();
+    }
+
+    public Compra(String code, User user, GregorianCalendar dataCompra, TipoPagamentoCompra tipoPagamentoCompra, double valorTotal, double valorPago) {
+        this.code = code;
+        this.user = user;
+        this.dataCompra = dataCompra;
+        this.tipoPagamentoCompra = tipoPagamentoCompra;
+        this.valorTotal = valorTotal;
+        this.valorPago = valorPago;
+        this.produtos = new LinkedList<>();
+        this.pagamentos = new HashSet<>();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public GregorianCalendar getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(GregorianCalendar dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public TipoPagamentoCompra getTipoPagamentoCompra() {
+        return tipoPagamentoCompra;
+    }
+
+    public void setTipoPagamentoCompra(TipoPagamentoCompra tipoPagamentoCompra) {
+        this.tipoPagamentoCompra = tipoPagamentoCompra;
+    }
+
+    public Set<Pagamento> getPagamentos() {
+        return pagamentos;
+    }
+
+    public void setPagamentos(Set<Pagamento> pagamentos) {
+        this.pagamentos = pagamentos;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Compra && ((Compra) o).code.equals(this.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, user, produtos, dataCompra, tipoPagamentoCompra, pagamentos, valorTotal, valorPago);
+    }
+}
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="PRODUTOS", uniqueConstraints = @UniqueConstraint(columnNames = {"TIPO"}))
+@Table(name="PRODUTOS")
 @NamedQueries({
         @NamedQuery(
                 name="getAllProdutos",
